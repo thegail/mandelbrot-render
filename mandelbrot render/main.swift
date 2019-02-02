bef619edd4daaa3e2fdd8b8ef148c2b6d4da6f89
Teddy Gaillard <teddy@teddys-macbook-air.local>
2019-02-02 08:16:02 -0800

    Initial Commit

---
+++ mandelbrot render/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  mandelbrot render
+//
+//  Created by Teddy Gaillard on 2/2/19.
+//  Copyright © 2019 thegail. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
