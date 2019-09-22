42f2c7d7c1ab6dd3c8578e3b9ec7fc61047876a3
Theodore Gaillard <teddy.gaillard@icloud.com>
2019-09-22 12:25:41 -0700

    Added import for complex

--- mandelbrot render/root.swift
+++ mandelbrot render/root.swift
@@ -7,51 +7,10 @@
 //
 
 import Foundation
-
-struct complex: CustomStringConvertible {
-	var real:Double
-	var imag:Double
-	
-	var description: String {
-		return "\(real)+\(imag)i"
-	}
-
-	func abs() -> Double { return (self.real*self.real+self.imag*self.imag).squareRoot() }
-	
-	func exp(_ p:Int) -> complex {
-		if p==1 {
-			return self
-		} else {
-			return self * self.exp(p-1)
-		}
-	}
-	
-	init(real:Double,imag:Double) {
-		self.real = real
-		self.imag = imag
-	}
-}
-
-extension complex {
-	static func + (left:complex, right:complex) -> complex {
-		let newr = left.real+right.real
-		let newi = left.imag+right.imag
-		return complex(real:newr,imag:newi)
-	}
-	static func - (left:complex, right:complex) -> complex {
-		let newr = left.real-right.real
-		let newi = left.imag-right.imag
-		return complex(real:newr,imag:newi)
-	}
-	static func * (left:complex, right:complex) -> complex {
-		let nr = (left.real*right.real)+(left.imag*right.imag * -1)
-		let ni = (left.real*right.imag)+(left.imag*right.real)
-		return complex(real: nr, imag: ni)
-	}
-}
+import complex
 
 func mandelbrot(RealComponent x:Double,ImaginaryComponent y:Double,MaxIterations i:Int, MultibrotStage stage:Int) -> Int {
-	let c = complex(real: x,imag: y)
+	let c = complex.Complex(real: x,imag: y)
 	var z = c
 	var pasti = 0
 	for n in 0...i {
